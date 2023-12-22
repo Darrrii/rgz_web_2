e94457a5ebd3c3f251f91771098f70f2d71c4e5b
Пушкарева Дарья <dasha.pushkareva.nsk@gmail.com>
2023-12-22 22:47:20 +0700

    rgz7

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
-from flask import Flask, redirect, url_for, render_template, Blueprint
+from flask import Flask
+from base import base
 from flask_sqlalchemy import SQLAlchemy
 from Db import db
-from Db.models import users, advertisement
-
+from Db.models import Users
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
+app.register_blueprint(base)
 
-app.secret_key = "123"
-user_db="daria_rgz"
-host_ip="127.0.0.1"
-host_port="5432"
-database_name='rgz_base'  
-password='123'
 
-app.config['SQLALCHEMY_DATABASE_URI']= f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
-app.config['SQALCHEMY_TRACK_MODIFICATIONS']= False
+app.secret_key = '123'
+user_db = 'dasha_knowledge_base_rgz'
+host_ip = '127.0.0.1'
+host_port = '5432'
+database_name = 'knowledge_base_rgz'
+password = '123'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'base.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_users(user_id):
+    return Users.query.get(int(user_id))
+
+with app.app_context():
+    db.create_all()
