fcaa987f2b92d56512672f28aa54bd2de0d84420
Пушкарева Дарья <dasha.pushkareva.nsk@gmail.com>
2023-12-22 22:47:00 +0700

    rgz6

--- Db/models.py
+++ Db/models.py
@@ -1,22 +1,32 @@
 from . import db
 from flask_login import UserMixin
 
-# Модель объявления
-class advertisement(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    topic = db.Column(db.String(100), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(50), nullable=False)
-
-def __repr__(self):
-        return f'id:{self.id}, username:{self.username}'
-
 # Модель пользователя
-class users(db.Model, UserMixin):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+# Модель товара
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    avatar = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    about = db.Column(db.Text)
+    code = db.Column(db.String(20), unique=True)
+    quantity = db.Column(db.Integer)
+    paid_quantity = db.Column(db.Integer, default=0)
+
+# Модель заказа
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    products = db.relationship('OrderProduct', backref='order', lazy=True)
+    is_paid = db.Column(db.Boolean, default=False)
+    is_draft = db.Column(db.Boolean, default=True)
+
+# Модель товара в заказе
+class OrderProduct(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer)
+    product = db.relationship('Product', backref='order_products')
